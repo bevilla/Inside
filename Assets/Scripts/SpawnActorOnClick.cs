fb47727aac3e1c8e79d696dd0bcb8a2d39986b7b
Thomas Trupin <thomas.trupin74@laposte.net>
2018-01-27 00:40:18 +0100

    Ajout du script de spawn on click

---
+++ Assets/Scripts/SpawnActorOnClick.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnActorOnClick : MonoBehaviour {
+
+   public  GameObject toInstantiate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit rayInfo;
+            if (Physics.Raycast(ray, out rayInfo))
+                Instantiate(toInstantiate, rayInfo.point, Quaternion.identity);
+        }
+    }
+}
