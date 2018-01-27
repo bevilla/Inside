032d3bb4df92e562f10e7fe8195ca50a1858c4af
Thomas Trupin <thomas.trupin74@laposte.net>
2018-01-27 03:16:15 +0100

    Layers towers et minion

--- Assets/Scripts/SpawnActorOnClick.cs
+++ Assets/Scripts/SpawnActorOnClick.cs
@@ -2,23 +2,59 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SpawnActorOnClick : MonoBehaviour {
+public enum E_Spawn
+{
+    NONE,
+    TOWER,
+    MINION
+};
 
-   public  GameObject toInstantiate;
+public class SpawnActorOnClick : MonoBehaviour
+{
+    public static SpawnActorOnClick instance;
+
+    public  GameObject toInstantiate;
+    public E_Spawn currentSelectedSpawnable = E_Spawn.NONE;
+    bool canSpawn = true;
 
 	// Use this for initialization
-	void Start () {
-		
+	void Start ()
+    {
+        SpawnActorOnClick.instance = this;
 	}
 
     public void Update()
     {
-        if (Input.GetButtonDown("Fire1"))
+        if (Input.GetButtonDown("Fire1") && canSpawn)
         {
             Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit rayInfo;
-            if (Physics.Raycast(ray, out rayInfo))
-                Instantiate(toInstantiate, rayInfo.point, Quaternion.identity);
+
+            if (currentSelectedSpawnable == E_Spawn.TOWER)
+            {
+                if (Physics.Raycast(ray, out rayInfo, 9999, LayerMask.GetMask("SpawnTower")))
+                    Instantiate(toInstantiate, rayInfo.point, Quaternion.identity);
+            }
+            else if (currentSelectedSpawnable == E_Spawn.MINION)
+            {
+                if (Physics.Raycast(ray, out rayInfo, 9999, LayerMask.GetMask("SpawnMinion")))
+                    Instantiate(toInstantiate, rayInfo.point, Quaternion.identity);
+            }
         }
+    }
+
+    public void changeObject(GameObject newObject)
+    {
+        toInstantiate = newObject;
+    }
+
+    public void changeObjectType(int type)
+    {
+        currentSelectedSpawnable = (E_Spawn)type;
+    }
+
+    public void canInstantiate(bool canOrCannotXd)
+    {
+        canSpawn = canOrCannotXd;
     }
 }
